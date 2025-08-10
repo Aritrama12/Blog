5822f3413ba2daaf9b3e19e506f5a6458f7c30cb
Aritra Manna <161790604+Aritrama12@users.noreply.github.com>
2025-08-09 20:45:20 -0700

    changes

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -80,7 +80,7 @@ function Register() {
         <div className="w-full max-w-md bg-white shadow-md rounded-lg p-8">
           <form onSubmit={handleRegister}>
             <div className="font-semibold text-xl items-center text-center">
-              Cilli<span className="text-blue-500">Blog</span>
+              Amazing<span className="text-blue-500">Blog</span>
             </div>
             <h1 className="text-xl font-semibold mb-6">Register</h1>
             <select
--- frontend/src/utils.js
+++
@@ -1,1 +0,0 @@
-export const BACKEND_URL="https://blogapplication-mtz0.onrender.com"
